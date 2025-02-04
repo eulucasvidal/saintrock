05e51a7ef0b5416ce30336febd20f70134832462
Lucas Vidal <luccas.lima12@gmail.com>
2025-02-04 17:45:31 -0300

    Alterando todas as estruturas de pastas do projeto

---
+++ src/js/global.js
@@ -0,0 +1,14 @@
+import initToggleMenu from "./menuMobile/menu-mobile.js";
+initToggleMenu();
+
+import initBannerSlider from "./bannerSlide/banner-slide.js";
+initBannerSlider();
+
+import initTenisDestaque from "./lancamentos/lancamentos-imgs.js";
+initTenisDestaque();
+
+import initModal from "./modal/modal.js";
+initModal();
+
+import initLetreiroAnimado from "./letreiroAnimado/letreiro-animado.js";
+initLetreiroAnimado();
